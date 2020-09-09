7495c284f26c736328576dbdf8d1f49dccd87825
wyneth <70347244+wyneth@users.noreply.github.com>
2020-09-09 16:03:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    volume_sphere = 4 / 3 * pi * radius ** 3
+    basic.showNumber(volume_sphere)
+})
+let volume_sphere = 0
+let pi = 0
+let radius = 0
+radius = 10
+pi = 3.1416
+basic.forever(function () {
+	
+})
